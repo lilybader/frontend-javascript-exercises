48b34eb4609f119532b93ea8af43d9ff5565838d
Lily Bader <baderlily@gmail.com>
2015-05-22 12:09:33 -0700

    Update challenge.js

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,27 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object) {
+	var newObject = Object.create(object); 
+	return newObject;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+	for (var property in src) {
+		if (src.hasOwnProperty(property)) {
+			dest[property] = src[property];
+		};
+	}; 
+	return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(object, elemArray) {
+	var objectKeys = Object.keys(object);
+	objectKeys.sort();
+	elemArray.sort();
+	for (i = 0; i < objectKeys.length; i++) {
+		if (objectKeys[i] == elemArray[i] || elemArray.length = 0) {
+			return true;
+		}
+		else {
+			return false;
+		};
+	};
+};
