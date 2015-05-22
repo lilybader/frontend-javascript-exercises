fc3c2d1e790153a2da7826d6555db0c0ad6355c8
Lily Bader <baderlily@gmail.com>
2015-05-22 11:53:21 -0700

    Update challenge.js

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,25 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+	var sum = 0;
+	for (var index = 0; index < array.length; index++) {
+		sum += array[index];
+	}; 
+	return sum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+	var newString = inputString.toLowerCase ();
+	var splitString = newString.split(",");
+	var newArray = []
+	for (var index = 0; index < splitString.length; index++) {
+		newArray.push(splitString[index]);
+	}; 
+	return newArray;
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(array) {
+  var newArray = [];
+  for (var index=0; index < array.length; index++) {
+    newArray.push(index + " is " + array[index]);
+  };
+  return newArray;
+};
