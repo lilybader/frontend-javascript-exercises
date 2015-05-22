a90f8f0009d7e64072eec1b4dcc834c80ded0fa2
Lily Bader <baderlily@gmail.com>
2015-05-22 11:52:47 -0700

    Update challenge.js

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,23 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object) {
+	var newArray = [];
+	for (var property in object) {
+		newArray.push(property);
+	}; 
+	return newArray;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(object) {
+	var valueArray = [];
+	for (var value in object) {
+		valueArray.push(object[value]);
+	}; 
+	return valueArray;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(object) {
+	var stringArray = [];
+	for (var property in object) {
+		stringArray.push(property + " is " + object[property]);
+	}; 
+	return stringArray;
+};
