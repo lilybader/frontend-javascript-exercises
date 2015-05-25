1fe2510fe33fdaf153efdd5df2c727ef187ed415
Lily Bader <baderlily@gmail.com>
2015-05-25 08:40:37 -0700

    solved while loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,15 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+	while (conditionalFn()) {
+		actionFn();
+	};
+};
 
-module.exports.sumNumbers = undefined;
-
+module.exports.sumNumbers = function (numArray) {
+	var counter = 0;
+	var sum = 0;
+	while (counter < numArray.length) {
+		sum += numArray[counter];
+		counter++;
+	};
+	return sum;
+};
